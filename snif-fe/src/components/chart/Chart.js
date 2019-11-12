e23087a8e5d42eb30951950732b70d003f96f074
DIogo Yaguas <diogo.yaguas@hotmail,com>
2019-11-12 01:45:57 +0000

    [Feat] Multi datasets charts

--- snif-fe/src/components/chart/Chart.js
+++ snif-fe/src/components/chart/Chart.js
@@ -1,78 +1,84 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import Chart from "chart.js";
 
 export default class LineGraph extends Component {
+  chartRef = React.createRef();
 
-    chartRef = React.createRef();
+  componentDidMount() {
+    const lineChart = this.chartRef.current.getContext("2d");
 
-    componentDidMount() {
-        const lineChart = this.chartRef.current.getContext("2d");
-        
-        Chart.defaults.global.defaultFontFamily = "Roboto";
+    Chart.defaults.global.defaultFontFamily = "Roboto, sans serif";
 
-        new Chart(lineChart, {
-            type: this.props.type,
-            data: {
-                labels: this.props.labels,
-                datasets: [
-                    {
-                        label: "Sales",
-                        data: this.props.data,
-                        backgroundColor: 'rgba(62,231,173, 0.3)',
-                        borderColor: 'rgba(62,231,173, 100)',
-                    }
-                ]
-            },
-            options: {
-                elements: {
-                    line: {
-                        tension: 0
-                    }
-                },
-                legend: {
-                    labels: {
-                        fontColor: "white",
-                        fontSize: 18
-                    }
-                },
-                responsive: true,
-                maintainAspectRatio: true,
-                scales: {
-                    xAxes: [{
-                        ticks: {
-                            fontColor: "white",
-                            fontSize: 18,
-                            autoSkip: false,
-                    maxRotation: 45,
-                    minRotation: 45
-                        },
-                        gridLines: {
-                            display: false,
-                            drawBorder: false
-                        }
-                    }],
-                    yAxes: [{
-                        ticks: {
-                            fontColor: "white",
-                            fontSize: 18,
-                        },
-                        gridLines: {
-                            color: "white",
-                        },
-                    }],
-                }
-            },
+    let datasets = [];
+    let counter = 0;
 
-        });
-    }
-    render() {
-        return (
-            <div >
-                <canvas
-                    id="lineChart"
-                    ref={this.chartRef}
-                />
-            </div>
-        )
-    }
+    const backgroundColor = ["rgba(62,231,173, 0.3)", "rgba(40,147,111,0.3)", "rgba(29,105,79,0.3)"];
+    const borderColor = ["rgba(62,231,173, 255)", "rgba(40,147,111,255)", "rgba(29,105,79,255)"];
+
+    Object.keys(this.props.datas).map(key => {
+        var dataset = {
+            label: key,
+            data: this.props.datas[key],
+            backgroundColor: backgroundColor[counter],
+            borderColor: borderColor[counter],
+            }
+        counter++;
+        datasets.unshift(dataset);
+    });
+
+    new Chart(lineChart, {
+      type: this.props.type,
+      data: {
+        labels: this.props.labels,
+        datasets: datasets,
+      },
+      options: {
+        legend: {
+          labels: {
+            fontColor: "white",
+            fontSize: 18
+          }
+        },
+        responsive: true,
+        maintainAspectRatio: true,
+        scales: {
+          xAxes: [
+            {
+              ticks: {
+                fontColor: "white",
+                fontSize: 18,
+                autoSkip: false,
+                maxRotation: 45,
+                minRotation: 45
+              },
+              gridLines: {
+                display: false,
+                drawBorder: false
+              }
+            }
+          ],
+          yAxes: [
+            {
+              ticks: {
+                fontColor: "white",
+                fontSize: 18
+              },
+              gridLines: {
+                color: "white"
+              }
+            }
+          ]
+        }
+      }
+    });
+  }
+
+  render() {
+
+    return (
+      <div>
+        <canvas id="lineChart" ref={this.chartRef} />
+      </div>
+    );
+  }
 }
