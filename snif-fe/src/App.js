a3170a84929024b653a3b517ef83bf7213b6627f
Antero Santos <up201604602@fe.up.pt>
2019-11-25 17:14:48 +0000

    shhhh

--- snif-fe/src/App.js
+++ snif-fe/src/App.js
@@ -2,12 +2,10 @@ import React from "react";
 import { Router } from "@reach/router";
 
 import Home from "./pages/Home";
-import Login from "./pages/Login";
 
 const App = () => (
     <Router>
         <Home path="/" />
-        <Login path="/login" />
     </Router>
 );
 
