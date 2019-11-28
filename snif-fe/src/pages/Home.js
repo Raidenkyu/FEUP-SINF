6e85b54c57842769a8644dd50e63cdd60c342cc3
Antero Santos <up201604602@fe.up.pt>
2019-11-28 18:29:50 +0000

    Landing page almost done

--- snif-fe/src/pages/Home.js
+++ snif-fe/src/pages/Home.js
@@ -1,15 +1,19 @@
 import React from "react";
-
-import Header from "../components/home/Header";
+import { Link } from "@reach/router";
 import Layout from "../components/common/Layout";
+import logo from "../../src/assets/logo.png";
 
 import HomeStyles from "../styles/home/Home.module.css";
 
 const Home = () => (
     <Layout navbar={false} sidebar={false}>
-        <div className={HomeStyles.home}>
-            <Header />
+        <Link to ="/" className={HomeStyles.link}>
+            Login
+        </Link>
+        <div className={HomeStyles.container}>
+            <svg className={HomeStyles.panel} ></svg>
         </div>
+        <img className={HomeStyles.logo} src={logo}></img>
     </Layout>
 );
 
