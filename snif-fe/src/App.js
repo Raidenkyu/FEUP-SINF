c758684d9625439e75b4fb842de3d2a3eb932f60
Antero Santos <up201604602@fe.up.pt>
2019-11-30 18:51:17 +0000

    Add basic structure

--- snif-fe/src/App.js
+++ snif-fe/src/App.js
@@ -2,10 +2,12 @@ import React from "react";
 import { Router } from "@reach/router";
 
 import Home from "./pages/Home";
+import Overview from "./pages/Overview";
 
 const App = () => (
     <Router>
         <Home path="/" />
+        <Overview path="/overview"/>
     </Router>
 );
 
---
+++ snif-fe/src/pages/Overview.js
@@ -0,0 +1,91 @@
+import React from "react";
+import Layout from "../components/common/Layout";
+import { Container, Row, Col, Card, CardHeader, CardBody } from 'reactstrap';
+import OverviewStyles from "../styles/overview/Overview.module.css";
+
+
+const Overview = () => (
+    <Layout navbar={true} sidebar={true}>
+        <Container>
+
+        <Row>
+            <Col lg="12">
+                <Card className={OverviewStyles.card}>
+                    <CardHeader>
+                        Balance
+                    </CardHeader>
+                    <CardBody>
+                        
+                    </CardBody>
+                </Card>
+            </Col>
+        </Row>
+        <Row>
+            <Col lg="4">
+                <Card className={OverviewStyles.card}>
+                    <CardHeader>                        
+                        Sales
+                    </CardHeader>
+                    <CardBody>
+                        
+                    </CardBody>
+                </Card>
+            </Col>
+            <Col lg="4">
+                <Card className={OverviewStyles.card}>
+                    <CardHeader>
+                        Purchases
+                    </CardHeader>
+                    <CardBody>
+                        
+                    </CardBody>
+                </Card>
+            </Col>
+            <Col lg="4">
+                <Card className={OverviewStyles.card}>
+                    <CardHeader>
+                        Stocks
+                    </CardHeader>
+                    <CardBody>
+                        
+                    </CardBody>
+                </Card>
+            </Col>
+        </Row>
+        <Row>
+            <Col lg="4">
+                <Card className={OverviewStyles.card}>
+                    <CardHeader>
+                        Orders
+                    </CardHeader>
+                    <CardBody>
+                        
+                    </CardBody>
+                </Card>
+            </Col>
+            <Col lg="4">
+                <Card className={OverviewStyles.card}>
+                    <CardHeader>
+                        Customers
+                    </CardHeader>
+                    <CardBody>
+                        
+                    </CardBody>
+                </Card>
+            </Col>
+            <Col lg="4">
+                <Card className={OverviewStyles.card}>
+                    <CardHeader>
+                        Financial
+                    </CardHeader>
+                    <CardBody>
+                        
+                    </CardBody>
+                </Card>
+            </Col>
+        </Row>
+        </Container>
+    </Layout>
+)
+
+export default Overview;
