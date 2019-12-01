e94c313251dfc15a4509a59af4ba5badaf76360d
DuarteOliveira8 <duartefmoliveira8@gmail.com>
2019-12-01 01:27:33 +0000

    feat: input component

---
+++ snif-fe/src/components/common/inputs/Input.js
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import { FormGroup, Label, Input } from "reactstrap";
+
+import InputStyles from "../../../styles/common/inputs/input.module.css";
+
+const InputCustom = ({ id, label, type, placeholder, options }) => {
+    const [focused, setFocused] = useState(false);
+
+    const handleFocus = () => {setFocused(true);}
+    const handleUnfocus = () => {setFocused(false);}
+
+    return (
+        <FormGroup className={focused ? InputStyles.inputFocused : InputStyles.inputUnfocused}>
+            {label !== null ? <Label for={id}>{label}</Label> : ""}
+            {type === "select" ?
+                <Input
+                    type={type}
+                    name={id}
+                    id={id}
+                    placeholder={placeholder}
+                    onFocus={handleFocus}
+                    onBlur={handleUnfocus}
+                    className={InputStyles.input}
+                >
+                    {options.map((option) => (
+                        <option value={option} key={option}>{option}</option>
+                    ))}
+                </Input>
+                :
+                <Input
+                    type={type}
+                    name={id}
+                    id={id}
+                    placeholder={placeholder}
+                    onFocus={handleFocus}
+                    onBlur={handleUnfocus}
+                    className={InputStyles.input}
+                />
+            }
+        </FormGroup>
+    )
+};
+
+InputCustom.propTypes = {
+    id: PropTypes.string.isRequired,
+    label: PropTypes.string,
+    type: PropTypes.string.isRequired,
+    placeholder: PropTypes.string,
+    options: PropTypes.array,
+};
+
+export default InputCustom;
--- snif-fe/src/pages/Login.js
+++ snif-fe/src/pages/Login.js
@@ -4,12 +4,10 @@ import Img from "react-image";
 import { Link } from "@reach/router";
 
 import Layout from "../components/common/Layout";
-// import { requestAccessToken }  from "../lib/api/jasmin";
+import Input from "../components/common/inputs/Input";
 
 import LoginStyles from "../styles/login/login.module.css";
 import Logo from "../assets/logo.png";
-
-// requestAccessToken();
 
 const Login = () => (
     <Layout navbar={false} sidebar={false}>
@@ -24,12 +22,22 @@ const Login = () => (
                 </Row>
                 <Row>
                     <Col xs={{ size: 6, offset: 3 }}>
-
+                        <Input
+                            id="email"
+                            label="E-mail:"
+                            type="text"
+                            placeholder="Insert your e-mail"
+                        />
                     </Col>
                 </Row>
                 <Row>
                     <Col xs={{ size: 6, offset: 3 }}>
-                        ola
+                        <Input
+                            id="password"
+                            label="Password:"
+                            type="password"
+                            placeholder="Insert your password"
+                        />
                     </Col>
                 </Row>
                 <Row>
