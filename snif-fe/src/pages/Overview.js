e1bf36cbdd69c6756c03800257b975595adc1d71
Duarte Oliveira <duartefmoliveira8@gmail.com>
2019-12-04 11:43:19 +0000

    refactor: content card component

--- snif-fe/src/pages/Overview.js
+++ snif-fe/src/pages/Overview.js
@@ -1,86 +1,51 @@
 import React from "react";
+import { Container, Row, Col } from "reactstrap";
+
 import Layout from "../components/common/Layout";
-import { Container, Row, Col, Card, CardHeader, CardBody } from "reactstrap";
-import OverviewStyles from "../styles/overview/Overview.module.css";
+import ContentCard from "../components/common/ismo/ContentCard"
 
 const Overview = () => (
     <Layout navbar={true} sidebar={true}>
         <Container>
-
             <Row>
                 <Col lg="12">
-                    <Card className={OverviewStyles.card}>
-                        <CardHeader>
-                        Balance
-                        </CardHeader>
-                        <CardBody>
-
-                        </CardBody>
-                    </Card>
+                    <ContentCard header="Balance">
+                        Estou?
+                    </ContentCard>
                 </Col>
             </Row>
             <Row>
                 <Col lg="4">
-                    <Card className={OverviewStyles.card}>
-                        <CardHeader>
-                        Sales
-                        </CardHeader>
-                        <CardBody>
-
-                        </CardBody>
-                    </Card>
+                    <ContentCard header="Sales">
+                        Camila?
+                    </ContentCard>
                 </Col>
                 <Col lg="4">
-                    <Card className={OverviewStyles.card}>
-                        <CardHeader>
-                        Purchases
-                        </CardHeader>
-                        <CardBody>
-
-                        </CardBody>
-                    </Card>
+                    <ContentCard header="Purchases">
+                        Chupa
+                    </ContentCard>
                 </Col>
                 <Col lg="4">
-                    <Card className={OverviewStyles.card}>
-                        <CardHeader>
-                        Stocks
-                        </CardHeader>
-                        <CardBody>
-
-                        </CardBody>
-                    </Card>
+                    <ContentCard header="Stocks">
+                        me
+                    </ContentCard>
                 </Col>
             </Row>
             <Row>
                 <Col lg="4">
-                    <Card className={OverviewStyles.card}>
-                        <CardHeader>
-                        Orders
-                        </CardHeader>
-                        <CardBody>
-
-                        </CardBody>
-                    </Card>
+                    <ContentCard header="Orders">
+                        a
+                    </ContentCard>
                 </Col>
                 <Col lg="4">
-                    <Card className={OverviewStyles.card}>
-                        <CardHeader>
-                        Customers
-                        </CardHeader>
-                        <CardBody>
-
-                        </CardBody>
-                    </Card>
+                    <ContentCard header="Customers">
+                        pi
+                    </ContentCard>
                 </Col>
                 <Col lg="4">
-                    <Card className={OverviewStyles.card}>
-                        <CardHeader>
-                        Financial
-                        </CardHeader>
-                        <CardBody>
-
-                        </CardBody>
-                    </Card>
+                    <ContentCard header="Financial">
+                        la
+                    </ContentCard>
                 </Col>
             </Row>
         </Container>
