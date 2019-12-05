384acb1911f7cc837b00e2b2d7515cf0894acde1
Fernando Alves <up201605270@fe.up.pt>
2019-12-05 23:19:19 +0000

    Added saft demo1 json object

--- saft-reader/src/index.js
+++ saft-reader/src/index.js
@@ -1,1 +1,8 @@
-console.log("START");
+const saft = require('../files/json/saft-demo1');
+
+startUp();
+
+function startUp () {
+    console.log("Saft Version:", saft.jsonObj.AuditFile.Header.AuditFileVersion);
+}
+
