22cb24ef21dc1a4388f510dfe2d800bbc50eb335
Fernando Alves <up201605270@fe.up.pt>
2019-12-06 22:43:53 +0000

    Ativo não passivo done

--- saft-reader/src/index.js
+++ saft-reader/src/index.js
@@ -94,11 +94,12 @@ function createBalanceSheet () {
     saft.AuditFile.MasterFiles.GeneralLedgerAccounts.Account.forEach((account) => {
         const accountId = account.AccountID;
         const accountBal = account.ClosingDebitBalance - account.ClosingCreditBalance;
-        const accountTaxCode = account.TaxonomyCode;
+        let accountTaxCode = account.TaxonomyCode;
         
         if (accountTaxCode === undefined)
             return;
-        
+
+        accountTaxCode = parseInt(accountTaxCode);
 
         switch (accountTaxCode) {
             case 268:
@@ -108,6 +109,10 @@ function createBalanceSheet () {
             case 272:
             case 273:
             case 274:
+            case 306:
+            case 310:
+                addValue(balanceSheet, ['Ativo', 'Ativo não corrente', 'Ativos fixos tangíveis'], accountBal);
+                break;
             case 275:
             case 276:
             case 277:
@@ -122,19 +127,182 @@ function createBalanceSheet () {
             case 286:
             case 287:
             case 288:
-            case 306:
-            case 310:
             case 314:
             case 318:
-                
+                addValue(balanceSheet, ['Ativo', 'Ativo não corrente', 'Ativos fixos tangíveis'], -accountBal);
                 break;
-        
+            //=======================//
+            case 259:
+            case 260:
+            case 261:
+            case 305:
+            case 309:
+                addValue(balanceSheet, ['Ativo', 'Ativo não corrente', 'Propriedades de investimento'], accountBal);
+                break;
+            case 262:
+            case 263:
+            case 264:
+            case 265:
+            case 266:
+            case 267:
+            case 313:
+            case 317:
+                addValue(balanceSheet, ['Ativo', 'Ativo não corrente', 'Propriedades de investimento'], -accountBal);
+                break;
+            //=======================//
+            case 217:
+            case 222:
+            case 227:
+            case 289:
+                addValue(balanceSheet, ['Ativo', 'Ativo não corrente', 'Goodwill'], accountBal);
+                break;
+            case 236:
+            case 237:
+            case 238:
+            case 240:
+            case 245:
+            case 250:
+            case 294:
+            case 299:
+                addValue(balanceSheet, ['Ativo', 'Ativo não corrente', 'Goodwill'], -accountBal);
+                break;
+            //=======================//
+            case 290:
+            case 291:
+            case 292:
+            case 293:
+            case 307:
+            case 311:
+                addValue(balanceSheet, ['Ativo', 'Ativo não corrente', 'Ativos intangíveis'], accountBal);
+                break;
+            case 295:
+            case 296:
+            case 297:
+            case 298:
+            case 300:
+            case 301:
+            case 302:
+            case 303:
+            case 315:
+            case 319:
+                addValue(balanceSheet, ['Ativo', 'Ativo não corrente', 'Ativos intangíveis'], -accountBal);
+                break;
+            //=======================//
+            case 197:
+            case 198:
+            case 215:
+                addValue(balanceSheet, ['Ativo', 'Ativo não corrente', 'Ativos biológicos'], accountBal);
+                break;
+            case 200:
+            case 202:
+                addValue(balanceSheet, ['Ativo', 'Ativo não corrente', 'Ativos biológicos'], -accountBal);
+                break;
+            //=======================//
+            case 216:
+            case 221:
+            case 226:
+                addValue(balanceSheet, ['Ativo', 'Ativo não corrente', 'Participações financeiras'], accountBal);
+                break;          
+            case 239:
+            case 244:
+            case 249:
+                addValue(balanceSheet, ['Ativo', 'Ativo não corrente', 'Participações financeiras'], -accountBal);
+                break;
+            //=======================//
+            case 218:
+            case 219:
+            case 220:
+            case 223:
+            case 224:
+            case 225:
+            case 228:
+            case 229:
+            case 230:
+            case 231:
+            case 232:
+            case 233:
+            case 234:
+            case 235:
+            case 304:
+            case 308:
+                addValue(balanceSheet, ['Ativo', 'Ativo não corrente', 'Outros investimentos financeiros'], accountBal);
+                break;
+            case 241:
+            case 242:
+            case 243:
+            case 246:
+            case 247:
+            case 248:
+            case 251:
+            case 252:
+            case 253:
+            case 254:
+            case 255:
+            case 256:
+            case 257:
+            case 258:
+            case 312:
+            case 316:
+                addValue(balanceSheet, ['Ativo', 'Ativo não corrente', 'Outros investimentos financeiros'], -accountBal);
+                break;
+            //=======================//
+            case 62:
+            case 64:
+            case 112:
+            case 114:
+            case 125:
+            case 127:
+            case 129:
+            case 139:
+                addValue(balanceSheet, ['Ativo', 'Ativo não corrente', 'Créditos a receber'], accountBal);
+                break;
+            case 68:
+            case 70:
+            case 121:
+            case 123:
+            case 141:
+            case 145:
+                addValue(balanceSheet, ['Ativo', 'Ativo não corrente', 'Créditos a receber'], -accountBal);
+                break;
+            //=======================//
+            case 133:
+                addValue(balanceSheet, ['Ativo', 'Ativo não corrente', 'Ativos por impostos diferidos'], accountBal);
+                break;              
+            case 143:       
+                addValue(balanceSheet, ['Ativo', 'Ativo não corrente', 'Ativos por impostos diferidos'], -accountBal);
+                break;
+            //=======================//
+
+
+
+            //=======================//
+
+
+            //=======================//
+
+
+            /* 
+            
+       
+
+
+
+
+            */
+
+            
+
+            //=======================//
+
             default:
+                console.log("Unhandled Taxonomy Code:", accountTaxCode);
                 break;
         }
 
     });
 
+
+    console.log(balanceSheet);
 
 
 
