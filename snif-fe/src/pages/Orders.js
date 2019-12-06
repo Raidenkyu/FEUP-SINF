cd03242a5e486c9784cd977b8c27e63df40fbd53
Antero Santos <up201604602@fe.up.pt>
2019-12-06 17:31:58 +0000

    New graph structure on purchases and orders

--- snif-fe/src/pages/Orders.js
+++ snif-fe/src/pages/Orders.js
@@ -12,7 +12,7 @@ const Orders = () => {
     const ordersChart = {
         type: "bar",
         labels: ["December 2018", "January 2019", "February 2019", "March 2019", "April 2019",
-        "May 2019", "June 2019", "July 2019", "August 2019", "September 2019", "October 2019", "November 2019"],
+            "May 2019", "June 2019", "July 2019", "August 2019", "September 2019", "October 2019", "November 2019"],
         datasets: {
             "cancelled": {
                 backgroundColor: colors.red.border,
