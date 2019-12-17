b15ac0cac42b92033d1df2cf20cba5864781c3c0
Fernando Alves <up201605270@fe.up.pt>
2019-12-17 14:25:03 +0000

    Added monthly expenses and diff

--- saft-reader/src/index.js
+++ saft-reader/src/index.js
@@ -1473,6 +1473,8 @@ function createOtherFinValues () {
 
     const finOverviewObject = {
         'monthlySales': getMonthlySalesObj(),
+        'monthlyExpenses': getMonthlyExpensesObj(),
+        'monthlyDiff': getMonthlyDiffObj(),
         'totalSales': getTotalSales(),
         'totalExpenses': getTotalExpenses(),
     }
@@ -1641,6 +1643,33 @@ function getMonthlySalesObj () {
 
     return salesValues;
 }
+
+function getMonthlyExpensesObj () {
+    const monthlyDR = global.monthlyResultsReport;
+    const expensesValues = [];
+
+    ['01','02','03','04','05','06','07','08','09','10','11','12'].forEach((month) => {
+        const auxCurrMonthSales = getPropVal(monthlyDR[month], '6');
+        
+        expensesValues.push(-auxCurrMonthSales);
+    });
+
+    return expensesValues;
+}
+
+function getMonthlyDiffObj () {
+    const monthlyDR = global.monthlyResultsReport;
+    const diffValues = [];
+
+    ['01','02','03','04','05','06','07','08','09','10','11','12'].forEach((month) => {
+        const auxCurrMonthDiff = getPropVal(monthlyDR[month], '1') - getPropVal(monthlyDR[month], '6');
+        
+        diffValues.push(auxCurrMonthDiff);
+    });
+
+    return diffValues;
+}
+
 
 function getTotalSales () {
     return getPropVal(global.anualResultsReport, '1');
