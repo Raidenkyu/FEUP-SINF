42cf97b396a1d6e96e306dc4db5ab50d1c338b55
Duarte Oliveira <duartefmoliveira8@gmail.com>
2019-12-18 19:24:35 +0000

    Purchases pagination done

---
+++ snif-fe/src/components/purchases/PurchasesList.js
@@ -0,0 +1,84 @@
+import React, { useState, useEffect } from "react";
+import Axios from "axios";
+
+import ContentCard from "../common/utils/ContentCard";
+import ContentTable from "../common/utils/ContentTable";
+
+const PurchasesList = () => {
+    const [loading, setLoading] = useState(true);
+    const [page, setPage] = useState(1);
+    const [purchasesRows, setPurchasesRows] = useState([]);
+    const [next, setNext] = useState(true);
+    const [previous, setPrevious] = useState(false);
+
+    const requestPurchases = (newPage) => {
+        Axios.get("http://localhost:9000/api/purchases/list?page=" + newPage + "&pageSize=15", {
+            headers: {
+                auth_token: localStorage.getItem("auth_token"),
+            },
+        }).then(({ data }) => {
+            setPurchasesRows(data.purchasesList);
+
+            if (data.purchasesList.length === 15) {
+                setNext(true);
+            } else {
+                setNext(false);
+            }
+
+            if (page === 1) {
+                setPrevious(false);
+            } else {
+                setPrevious(true);
+            }
+
+            setLoading(false);
+        }).catch(() => {
+            setLoading(false);
+        })
+    }
+
+    const handleNext = () => {
+        setLoading(true);
+        setPage(page+1);
+    }
+
+    const handlePrevious = () => {
+        if (page - 1 >= 1) {
+            setLoading(true);
+            setPage(page-1);
+        }
+    }
+
+    const handleReset = () => {
+        setLoading(true);
+        setPage(1);
+    }
+
+    useEffect(() => {
+        requestPurchases(page);
+    }, [page]);
+
+    const purchasesHeaders = [
+        { index: "purchaseId", value: "Purchase id" },
+        { index: "name", value: "Product" },
+        { index: "quantity", value: "Quantity" },
+        { index: "value", value: "Value (€)" },
+        { index: "date", value: "Date" },
+    ];
+
+    return (
+        <ContentCard loading={loading} header="Purchases">
+            <ContentTable
+                headers={purchasesHeaders}
+                rows={purchasesRows}
+                handlePrevious={handlePrevious}
+                previous={previous}
+                handleReset={handleReset}
+                handleNext={handleNext}
+                next={next}
+            />
+        </ContentCard>
+    );
+};
+
+export default PurchasesList;
--- snif-fe/src/pages/Purchases.js
+++ snif-fe/src/pages/Purchases.js
@@ -3,66 +3,33 @@ import { Container, Row, Col } from "reactstrap";
 import PropTypes from "prop-types";
 
 import Layout from "../components/common/layout/Layout";
-import ContentCard from "../components/common/utils/ContentCard";
-import ContentTable from "../components/common/utils/ContentTable";
 import Monthly from "../components/purchases/Monthly";
 import CumulativeMonthly from "../components/purchases/CumulativeMonthly";
 import Debt from "../components/purchases/Debt";
 import TopSuppliers from "../components/purchases/TopSuppliers";
+import PurchasesList from "../components/purchases/PurchasesList";
 
-const Purchases = ({ path }) => {
-
-    const purchasesHeaders = [
-        { index: "purchaseId", value: "Purchase id" },
-        { index: "product", value: "Product" },
-        { index: "quantity", value: "Quantity" },
-        { index: "value", value: "Value (€)" },
-        { index: "date", value: "Date" },
-    ];
-
-    const purchasesRows = [
-        { purchaseId: "4FN2SNB3", product: "Paper", quantity: "30.000", value: "1.500", date: "27/10/2019" },
-        { purchaseId: "4FN2SNB3", product: "Paper", quantity: "30.000", value: "1.500", date: "27/10/2019" },
-        { purchaseId: "4FN2SNB3", product: "Paper", quantity: "30.000", value: "1.500", date: "27/10/2019" },
-        { purchaseId: "4FN2SNB3", product: "Paper", quantity: "30.000", value: "1.500", date: "27/10/2019" },
-        { purchaseId: "4FN2SNB3", product: "Paper", quantity: "30.000", value: "1.500", date: "27/10/2019" },
-        { purchaseId: "4FN2SNB3", product: "Paper", quantity: "30.000", value: "1.500", date: "27/10/2019" },
-        { purchaseId: "4FN2SNB3", product: "Paper", quantity: "30.000", value: "1.500", date: "27/10/2019" },
-        { purchaseId: "4FN2SNB3", product: "Paper", quantity: "30.000", value: "1.500", date: "27/10/2019" },
-        { purchaseId: "4FN2SNB3", product: "Paper", quantity: "30.000", value: "1.500", date: "27/10/2019" },
-        { purchaseId: "4FN2SNB3", product: "Paper", quantity: "30.000", value: "1.500", date: "27/10/2019" },
-        { purchaseId: "4FN2SNB3", product: "Paper", quantity: "30.000", value: "1.500", date: "27/10/2019" },
-        { purchaseId: "4FN2SNB3", product: "Paper", quantity: "30.000", value: "1.500", date: "27/10/2019" },
-        { purchaseId: "4FN2SNB3", product: "Paper", quantity: "30.000", value: "1.500", date: "27/10/2019" },
-        { purchaseId: "4FN2SNB3", product: "Paper", quantity: "30.000", value: "1.500", date: "27/10/2019" },
-        { purchaseId: "4FN2SNB3", product: "Paper", quantity: "30.000", value: "1.500", date: "27/10/2019" },
-        { purchaseId: "4FN2SNB3", product: "Paper", quantity: "30.000", value: "1.500", date: "27/10/2019" },
-    ];
-
-    return (
-        <Layout navbar sidebar path={path}>
-            <Container>
-                <Monthly />
-                <Row className="mb-5">
-                    <Col xs="6">
-                        <Debt />
-                        <CumulativeMonthly />
-                    </Col>
-                    <Col xs="6">
-                        <TopSuppliers />
-                    </Col>
-                </Row>
-                <Row>
-                    <Col xs="12">
-                        <ContentCard header="Purchases">
-                            <ContentTable headers={purchasesHeaders} rows={purchasesRows} />
-                        </ContentCard>
-                    </Col>
-                </Row>
-            </Container>
-        </Layout>
-    );
-};
+const Purchases = ({ path }) => (
+    <Layout navbar sidebar path={path}>
+        <Container>
+            <Monthly />
+            <Row className="mb-5">
+                <Col xs="6">
+                    <Debt />
+                    <CumulativeMonthly />
+                </Col>
+                <Col xs="6">
+                    <TopSuppliers />
+                </Col>
+            </Row>
+            <Row>
+                <Col xs="12">
+                    <PurchasesList />
+                </Col>
+            </Row>
+        </Container>
+    </Layout>
+);
 
 Purchases.propTypes = {
     path: PropTypes.string.isRequired,
