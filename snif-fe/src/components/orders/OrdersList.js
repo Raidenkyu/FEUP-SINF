e79264d4096233126d1054a11f6efa24aefd8ed2
Antero Santos <up201604602@fe.up.pt>
2019-12-19 23:51:27 +0000

    Sales and Orders pages corrected
    In the Sales page there is one drill-down that currently is not working but since this will be changed Ill leave it like this for now

--- snif-fe/src/components/orders/OrdersList.js
+++ snif-fe/src/components/orders/OrdersList.js
@@ -2,7 +2,7 @@ import React from "react";
 
 import PaginatedTable from "../common/utils/PaginatedTable";
 
-const OrdersList = () => {
+const OrdersList = ({onRowClick}) => {
     const productHeaders = [
         { index: "id", value: "Order id" },
         { index: "product", value: "Product" },
@@ -18,6 +18,7 @@ const OrdersList = () => {
             headers={productHeaders}
             pageSize={15}
             list="ordersProducts"
+            onRowClick={onRowClick}
         />
     );
 };
