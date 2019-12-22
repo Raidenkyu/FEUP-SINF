fc3dcc7bd58698e0dfd2d91a5f456d22d2cc7472
João Carlos Maduro <joao-carlos.14@hotmail.com>
2019-12-22 18:45:01 +0000

    feat: Add transactions

--- snif-be/src/utils/stock.js
+++ snif-be/src/utils/stock.js
@@ -23,8 +23,57 @@ const getStockValue = (item) => {
     }, 0);
 }
 
+const getItemSales = (itemKey, sales) => {
+
+    const filteredSales = [];
+
+    sales.forEach((sale) => {
+        sale.documentLines.forEach((product) => {
+            if (product.salesItem == itemKey) {
+                filteredSales.push({
+                    id: product.orderId,
+                    product: product.description,
+                    customer: sale.buyerCustomerPartyName,
+                    state: (product.isDeleted ? "Cancelled" : (product.documentLineStatus == 1 ? "Pending" : "Processed")),
+                    quantity: product.quantity,
+                    value: product.lineExtensionAmount.amount,
+                    date: product.deliveryDate.split("T")[0]
+                });
+            }
+        });
+
+    });
+
+    return filteredSales;
+}
+
+const getItemPurchases = (itemKey, purchases) => {
+    const filteredPurchases = [];
+
+    purchases.forEach((purchase) => {
+        purchase.documentLines.forEach((product) => {
+            if (product.purchasesItem == itemKey) {
+                filteredPurchases.push({
+                    id: product.orderId,
+                    product: product.description,
+                    supplier: purchase.sellerSupplierPartyName,
+                    state: (product.isDeleted ? "Cancelled" : (product.documentLineStatus == 1 ? "Pending" : "Processed")),
+                    quantity: product.quantity,
+                    value: product.lineExtensionAmount.amount,
+                    date: product.deliveryDate.split("T")[0]
+                });
+            }
+        });
+
+    });
+
+    return filteredPurchases;
+}
+
 module.exports = {
     getStockQuantity,
     getUnitPrice,
-    getStockValue
+    getStockValue,
+    getItemSales,
+    getItemPurchases
 };
